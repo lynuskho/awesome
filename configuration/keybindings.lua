545c5cb9e6f4b8e241e4a2739b7148e50881106c
Lynus Kho <lynusgarrett@gmail.com>
2024-07-31 19:11:34 +0800

    bind super+ctrl+H to increase width to left, same for super+ctrl+L

--- configuration/keybindings.lua
+++ configuration/keybindings.lua
@@ -82,10 +82,10 @@ local function set_keybindings()
 			awful.client.urgent.jumpto,
 			{ description = "jump to urgent client", group = "client" }
 		),
-		awful.key({ modkey }, "l", function()
+		awful.key({ modkey, "Control" }, "l", function()
 			awful.tag.incmwfact(0.05)
 		end, { description = "increase master width factor", group = "layout" }),
-		awful.key({ modkey }, "h", function()
+		awful.key({ modkey("Control") }, "h", function()
 			awful.tag.incmwfact(-0.05)
 		end, { description = "decrease master width factor", group = "layout" }),
 		awful.key({ modkey, "Shift" }, "h", function()
@@ -94,12 +94,12 @@ local function set_keybindings()
 		awful.key({ modkey, "Shift" }, "l", function()
 			awful.tag.incnmaster(-1, nil, true)
 		end, { description = "decrease the number of master clients", group = "layout" }),
-		awful.key({ modkey, "Control" }, "h", function()
-			awful.tag.incncol(1, nil, true)
-		end, { description = "increase the number of columns", group = "layout" }),
-		awful.key({ modkey, "Control" }, "l", function()
-			awful.tag.incncol(-1, nil, true)
-		end, { description = "decrease the number of columns", group = "layout" }),
+		-- awful.key({ modkey, "Control" }, "h", function()
+		-- 	awful.tag.incncol(1, nil, true)
+		-- end, { description = "increase the number of columns", group = "layout" }),
+		-- awful.key({ modkey, "Control" }, "l", function()
+		-- 	awful.tag.incncol(-1, nil, true)
+		-- end, { description = "decrease the number of columns", group = "layout" }),
 		awful.key({ modkey }, " ", function()
 			awful.layout.inc(1)
 		end, { description = "select next", group = "layout" }),
